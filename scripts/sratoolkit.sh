4b203ce2527511ee3a680c1d767d23a918e17a13
Atemia <mulamajose@gmail.com>
2019-07-13 12:51:57 +0300

    added resource

--- scripts/sratoolkit.sh
+++ scripts/sratoolkit.sh
@@ -5,5 +5,5 @@ do
     # https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=toolkit_doc&f=fastq-dump
     # add the -X 5 option to make sure things are working as a test (only dumps 5 reads per file)
 
-    fastq-dump -X 2 --split-files --gzip -I -O /home/angela/ncbi $SAMPLE
+    fastq-dump -X 2 --split-files --gzip -I --outdir /home/joe/Eanbit_Project/Data $SAMPLE
 done
